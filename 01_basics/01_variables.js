bd7a36993b8e793b4be9484ff813d623b63e36ab
sohayamin <64743588+sohayamin@users.noreply.github.com>
2024-02-04 09:31:23 +0000

    second

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,8 @@
 const acountId = 144553
 let accountEmail = "hitesh@gmail.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+//accountCity = "Jaipur"
 
-accountId = 2
+//accountId = 2
 
 console.log(accountId)
