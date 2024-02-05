6d171470ceff196d57e73ff9781a55498cc011cf
sohayamin <64743588+sohayamin@users.noreply.github.com>
2024-02-05 07:05:45 +0000

    third

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,5 +19,25 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//**********Operations**********/
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2%2)
+
+let str1 = "Hitesh"
+let str2 = " chaudhary"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1"+2) 
---
+++ datatypes-summary.js
@@ -0,0 +1,20 @@
+// *primitive datatypes
+
+// 7 types: String, Number, Boolean, null, undefined, BigInt
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+const heroes = ["shakiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Soha",
+    age: 32
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
